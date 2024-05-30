f1bf763ea8eaac7ef8c16dbfc58d41298a2e2870
Toan Huynh <toan.ht2411@gmail.com>
2024-05-30 18:46:06 +0700

    chore: Add an option for expanded SASS output style

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,6 +5,8 @@ const { merge } = require("webpack-merge")
 const paths = require("./paths")
 const common = require("./webpack.common")
 
+const isOutputExpanded = process.env.SASS_STYLE === "expanded"
+
 module.exports = merge(common, {
   mode: "production",
   devtool: false,
@@ -12,24 +14,35 @@ module.exports = merge(common, {
     path: paths.build,
     publicPath: "./",
     filename: "js/[name].bundle.js",
+    clean: true,
   },
 
   module: {
     rules: [
       {
-        test: /\.(sass|scss|css)$/i,
+        test: /\.(sa|sc|c)ss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           {
             loader: "css-loader",
             options: {
-              importLoaders: 2,
+              importLoaders: 1,
               sourceMap: false,
               modules: false,
             },
           },
           "postcss-loader",
-          "sass-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: false,
+              sassOptions: {
+                outputStyle: isOutputExpanded ? "expanded" : "compressed",
+              },
+            },
+          },
         ],
       },
     ],
@@ -37,7 +50,7 @@ module.exports = merge(common, {
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles/[name].min.css",
+      filename: isOutputExpanded ? "styles/[name].css" : "styles/[name].min.css",
       chunkFilename: "[id].css",
     }),
   ],
@@ -46,9 +59,11 @@ module.exports = merge(common, {
     minimize: true,
     minimizer: [
       "...",
-      new CssMinimizerPlugin({
-        parallel: true,
-      }),
+      isOutputExpanded
+        ? null
+        : new CssMinimizerPlugin({
+            parallel: true,
+          }),
     ],
     runtimeChunk: "single",
     splitChunks: {
